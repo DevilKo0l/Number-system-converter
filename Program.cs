bfbf8992529bad109ee052eb4e6e00d4cd3d0f4a
Trung Nguyen <nguyen.tcao96@gmail.com>
2019-12-10 18:19:22 +0100

    Add function to check numeral system

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Number_system_converter
 {
@@ -11,9 +12,55 @@ namespace Number_system_converter
         static void Main(string[] args)
         {
 
+            Console.WriteLine(WhatBase("0101010"));
+            
+            
+        }
 
-            
-            
+        static public string WhatBase(string numInput)
+        {
+            if (IsHex(numInput) == true)
+            {
+                return "this is hexa num";
+            }
+            else if (IsBinary(numInput) == true)
+            {
+                return "this is binary number";
+            }
+            else if (IsOctal(numInput) == true)
+            {
+                return "this is octa number";
+            }
+            else
+            {
+                if (int.TryParse(numInput, out int result) == true)
+                {
+                    return "this is decimal";
+                }
+                else
+                {
+                    return "invalid input";
+                }
+            }
+
+
+        }
+
+        static public bool IsHex(string numInput)
+        {
+            //start with 0x or 0X, and @"\A\b^(0[xX])?[0-9a-fA-F]+\b\Z"
+            return Regex.IsMatch(numInput, @"^(0[xX])&([0-9a-fA-F])");
+
+        }
+
+        static public bool IsBinary(string numInput)
+        {
+            return Regex.IsMatch(numInput, @"^[01]+$");
+        }
+
+        static public bool IsOctal(string numInput)
+        {
+            return Regex.IsMatch(numInput, @"^(0)&([0-7])");
         }
 
         static public string DecimalToAnySystem(int numDecimal, int systembase)
@@ -33,5 +80,8 @@ namespace Number_system_converter
 
             return binary;
         }
+
+
+
     }
 }
